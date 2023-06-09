5b2b7fb60210b5117ccb2ccb31400312343a71ed
apurbojamananis <apurbojaman.anis@gmail.com>
2023-06-10 02:06:48 +0600

    get all user data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 //Mongodb
 const uri = `mongodb+srv://${process.env.SECRET_USERNAME}:${process.env.SECRET_KEY}@cluster0.orqgdcn.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,6 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("summerCamp").collection("users");
+
+    // user
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "User Already Exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
